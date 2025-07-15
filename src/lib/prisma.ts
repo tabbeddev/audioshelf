3404d8891922a9bb469bfbeb1cf7ccb1851006da
tabbeddev <83753407+tabbeddev@users.noreply.github.com>
2025-07-15 11:55:06 +0200

    Fixed build error

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,8 +3,3 @@ const { PrismaClient } = pkg;
 export const { Prisma } = pkg;
 
 export const db = new PrismaClient();
-
-globalThis.addEventListener("unload", async () => {
-  console.log("Goodbye");
-  await db.$disconnect();
-});
